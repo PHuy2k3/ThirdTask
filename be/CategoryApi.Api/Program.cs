750dedeb128b9e429b0bba0ade61e176d5ac0320
Huy <nguyenphanhuy1803@gmail.com>
2025-09-18 14:16:15 +0700

    push

--- be/CategoryApi.Api/Program.cs
+++ be/CategoryApi.Api/Program.cs
@@ -1,5 +1,7 @@
 ﻿using CategoryApi.Biz.Services;
 using CategoryApi.Data;
+using CategoryApi.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ const string CorsPolicy = "FE";
 builder.Services.AddCors(opt =>
 {
     opt.AddPolicy(CorsPolicy, p => p
-        .WithOrigins("http://localhost:3000","https://localhost:3000")
+        .WithOrigins("http://localhost:3000", "https://localhost:3000")
         .AllowAnyHeader()
         .AllowAnyMethod());
 });
@@ -16,7 +18,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddData(builder.Configuration);
+var provider = (builder.Configuration["DbProvider"] ?? "SqlServer").ToLowerInvariant();
+var cs = builder.Configuration.GetConnectionString("DefaultConnection")
+         ?? throw new InvalidOperationException("Missing ConnectionStrings:DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    if (provider == "mysql")
+        opt.UseMySql(cs, ServerVersion.AutoDetect(cs));
+    else
+        opt.UseSqlServer(cs);
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICatalogService, CatalogService>();
 builder.Services.AddAutoMapper(typeof(CategoryApi.Biz.Mapping.CategoryProfile).Assembly);
